0cb28a3e044beb68f595c37d816366229beb8d4d
bfreiberger <brunohfreiberger@gmail.com>
2019-08-13 01:30:47 -0300

    Initial structure, maven with swagger, modelmapper, jpa/hibernate, querydsl and more

---
+++ senior/src/main/java/com/br/senior/model/Cidade.java
@@ -0,0 +1,13 @@
+package com.br.senior.model;
+
+public class Cidade {
+	
+	private Long id;
+	private String Uf;
+	private String name;
+	private String capital;
+	private String longetude;
+	private String latitude;
+	private String nomesAlternativos;
+	
+}
