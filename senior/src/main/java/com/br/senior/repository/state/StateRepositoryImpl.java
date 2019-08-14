ed2e6574e116a7bec32ba7ad33568a124abe6f57
bfreiberger <brunohfreiberger@gmail.com>
2019-08-14 13:17:25 -0300

    adjusts

--- senior/src/main/java/com/br/senior/repository/state/StateRepositoryImpl.java
+++ senior/src/main/java/com/br/senior/repository/state/StateRepositoryImpl.java
@@ -1,12 +1,5 @@
 package com.br.senior.repository.state;
 
-import javax.persistence.EntityManager;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class StateRepositoryImpl implements StateCustomRepository {
-	
-	@Autowired
-	private EntityManager em;
-	
+		
 }
